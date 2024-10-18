3bdb82930d26dbfc22f1a489dcb0c1ed281f7e44
Mahir <mkojak75@gmail.com>
2024-10-19 00:29:08 +0200

    s2

---
+++ 02_intro/src/app.ts
@@ -0,0 +1,16 @@
+console.log("Hello");
+
+//! Classes Beispiel
+
+// class Employee {
+//   empCode: number;
+//   empName: string;
+
+//   constructor(code: number, name: string) {
+//     this.empCode = code;
+//     this.empName = name;
+//   }
+// }
+// let emp1 = new Employee(1, "Markus")
+
+// console.log(emp1);
